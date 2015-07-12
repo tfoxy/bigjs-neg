24fc2f19a8475a9bb68d0fa28232250fcdc901de
Tomás Fox <tomas.c.fox@gmail.com>
2015-07-12 12:54:51 -0300

    Modify test/.jshintrc file

--- test/test.js
+++ test/test.js
@@ -5,17 +5,17 @@ require('..')(Big);
 describe('bigjs-neg', function() {
   'use strict';
 
-  it('adds the neg method to a Big instance', function () {
+  it('adds the neg method to a Big instance', function() {
     var two = new Big(2);
     expect(two).to.have.property('neg').to.be.a('function');
   });
 
-  it('negates a positive number when neg method is called', function () {
+  it('negates a positive number when neg method is called', function() {
     var two = new Big(2);
     expect(two.neg()).to.deep.equal(new Big(-2));
   });
 
-  it('negates a negative number when neg method is called', function () {
+  it('negates a negative number when neg method is called', function() {
     var minusThree = new Big(-3);
     expect(minusThree.neg()).to.deep.equal(new Big(3));
   });
